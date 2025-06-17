99339fd285b1f09604eb44103c8cc18ba33edb4e
Bartek <bartolome@spoko.pl>
2025-06-17 14:30:45 +0200

    Dodanie obsługi argumentu --logs

--- skrypt.sh
+++ skrypt.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
 echo "Wywołaniie skryptu"
+
 export LC_TIME=pl_PL.UTF-8
 if [[ "$1" == "--date" ]]; then
     echo "Dzisiejsza data: $(date)"
+fi
+
+if [[ "$1" == "--logs" ]]; then
+    echo "Tworzenie plików log"
+    for i in {1..100}; do
+        filename="log${i}.txt"
+        echo -e "Nazwa pliku: $filename\nNazwa skryptu: $(basename "$0")\nData utworzenia: $(date)" > "$filename"
+    done
 fi
