5b48bad2bb091eadc092492e3ac0a9e86e0dd000
Bartek <bartolome@spoko.pl>
2025-06-17 14:42:09 +0200

    Dodanie obsugi  argumentu liczby dla --logs i przyjęcie domyslnej wartości 100

--- skrypt.sh
+++ skrypt.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-echo "Wywołaniie skryptu"
+echo "Wywołanie skryptu"
 
 export LC_TIME=pl_PL.UTF-8
+
 if [[ "$1" == "--date" ]]; then
     echo "Dzisiejsza data: $(date)"
 fi
 
 if [[ "$1" == "--logs" ]]; then
-    echo "Tworzenie plików log"
-    for i in {1..100}; do
+    count=100
+    if [[ -n "$2" && "$2" =~ ^[0-9]+$ ]]; then
+        count=$2
+    fi
+    echo "Tworzenie $count plików log"
+    for ((i=1; i<=count; i++)); do
         filename="log${i}.txt"
         echo -e "Nazwa pliku: $filename\nNazwa skryptu: $(basename "$0")\nData utworzenia: $(date)" > "$filename"
     done
